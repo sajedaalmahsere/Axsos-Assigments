0d4a61bf0a2b79526df8357775f7536c107ca180
Sajeda Salim <sajedah.almahsere@gmail.com>
2026-09-12 17:32:28 +0300

    Flex Navbar

--- Assignments/Web Fun/JS/Button Clicker/main.js
+++ Assignments/Web Fun/JS/Button Clicker/main.js
@@ -2,9 +2,17 @@ function Remove(element){
     element.remove();
 }
 
+
 function Change(button){
     button.textContent = "Logout";
+    
+    if (button.textContent == "Logout"){
+        button.textContent = "Login";
+        console.log(button)
+    }
 }
+
+
 
 function showAlert(){
     alert("ninja was liked");
--- Assignments/Web Fun/JS/Likes/script.js
+++ Assignments/Web Fun/JS/Likes/script.js
@@ -1,8 +1,26 @@
-var likes = document.querySelector("div label")
+
 
 function increase(){
+    var likes = document.querySelector("#box1")
     let int = parseInt(likes.innerText)
     var num  = int + 1
     likes.innerText = num + " like(s)"
 }
+
+function increase2(){
+    var likes = document.querySelector("#box2")
+    let int = parseInt(likes.innerText)
+    var num  = int + 1
+    likes.innerText = num + " like(s)"
+}
+
+
+function increase3(){
+    var likes = document.querySelector("#box3")
+    let int = parseInt(likes.innerText)
+    var num  = int + 1
+    likes.innerText = num + " like(s)"
+}
+
+
 
