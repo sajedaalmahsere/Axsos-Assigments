038f819c84920fc82cc9ffa634aabc4e2d3dcd75
Sajeda Salim <sajedah.almahsere@gmail.com>
2026-09-13 17:52:24 +0300

    Asignments

--- Assignments/Web Fun/JS/Practice Debugging/main.js
+++ Assignments/Web Fun/JS/Practice Debugging/main.js
@@ -6,21 +6,21 @@ var student = {
     totalLessons: 5,
 
 
-getProgressMessage: function(){
+getProgressMessage : function(){
     var percentage = (this.completedLessons / this.totalLessons)* 100;
-    return this.firstName + "finished" + percentage + "% of the lessons.";
+    return this.firstName + " finished " + percentage + "% of the lessons.";
     
     }
 };
 
 function printMessage(message) {
-console.log( "MESSAGE:" + message.toUpperCase());
+console.log( "MESSAGE: " + message.toUpperCase());
 }
 
 
-console.log(getLearningPlan(trainee));
+//console.log(getLearningPlan(trainee));
 
-printMessage(getLearningPlan(student));
+printMessage(student.getProgressMessage(student));
 
-const progressMessage = student.getProgressMessage;
+const progressMessage = student.getProgressMessage.bind(student);
 console.log(progressMessage());
