2211201c034ad07abecc342db97aa1dcaa75c4e9
Sajeda Salim <sajedah.almahsere@gmail.com>
2026-09-19 17:24:11 +0300

    Asignment

---
+++ Assignments/Web Fun/JS/Making Connections/main.js
@@ -0,0 +1,18 @@
+function userName(){
+    let settings = document.querySelector("#ch-name")
+    settings.innerHTML = "New Text"
+}
+
+function remove(){
+    let hide = document.querySelector("#person")
+    hide.remove();
+}
+
+// function remove(){
+//     let hide = document.querySelector(".Phil")
+//     hide.remove();
+// }
+
+
+
+
