93e4f261e5fad5eb72c6fa3cf2a01ed20f6761c9
Sajeda Salim <sajedah.almahsere@gmail.com>
2026-09-24 14:41:30 +0300

    python

---
+++ python_stack/pythonstack_/hello_world.py
@@ -0,0 +1,14 @@
+print("Hello world")
+
+name="Sajeda"
+print("Hello " , name)
+print("Hello " + name)
+
+num = 26
+print(f"Hello {num}")
+# print(f"Hello " + {num}) 
+
+fav_food1= "sushi"
+fav_food2="pizza"
+print("I love to eat "+fav_food1+" and "+ fav_food2)
+print(f"I love to eat {fav_food1} and {fav_food2}")
